dedbb66628f879a811c157c33d9cd7422fe9dcd3
lotf <federico.lot11@gmail.com>
2022-11-04 12:07:59 +0100

    added separation and fill distance

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <math.h>
 #include <cblas.h>
-
-//separation distance and fill distance
-
-FILE *file_pointer;
 
 unsigned int prod (unsigned int end, unsigned int begin){
     if (begin == end){
@@ -109,14 +106,87 @@ double wendland_function(double r, double k, int d) {
     }
 }
 
-double separation_distance(double * points);
+double separation_distance(double ** points, unsigned int dim_, unsigned int number_of_points_){
+    // printf("separation distance routine with:\n points dim: %d, points numbers: %d\n", dim_, number_of_points_);
+    double minimum = 1;
+    for (int i=0; i<number_of_points_-1; i++){
+        for (int j=i+1; j<number_of_points_; j++) {
+            // printf("%d,%d\n",i,j);
+            double temp[dim_];
+            for (int d=0; d<dim_; d++){
+                temp[d] = points[i][d] - points[j][d];
+            }
+            // printf("[%f, %f]\n", temp[0], temp[1]);
+            double p_dist = cblas_dnrm2((int) dim_, temp, 1);
+            // printf("%f\n",p_dist);
+            if (p_dist<minimum){
+                minimum = p_dist;
+            }
+        }
+    }
+    return minimum;
+}
 
-double fill_distance;
+double fill_distance(double ** points, unsigned int dim_, unsigned int number_of_points_, double threshold){
+    double min_vector[dim_], max_vector[dim_];
+    int partial_temp_points[dim_+1];
+    int total_temp_points = 1;
+    partial_temp_points[0] = 1;
+    for (int d=0; d<dim_; d++) {
+        min_vector[d] = 0;
+        max_vector[d] = 1;
+        /* if not in the standard settings [0,1]^d
+        min_vector[d] = points[0][d];
+        max_vector[d] = points[0][d];
+        for (int i=0; i<number_of_points_; i++){
+            if (points[i][d] < min_vector[d]){
+                min_vector[d]= points[i][d];
+            }
+            else if (points[i][d] > max_vector[d]){
+                max_vector[d] = points[i][d];
+            }
+        }*/
+        total_temp_points *= floor((max_vector[d]-min_vector[d])/threshold+1);
+        partial_temp_points[d+1] = total_temp_points;
+    }
+    double temp_grid_point[dim_];
+    double maximum = 0;
+    for (int temp_i=0; temp_i < total_temp_points; temp_i++){
+        // test the grid point generator: printf("[ "); for (int d=0; d<dim_; d++) {printf("%f ", temp_grid_point[d]);} printf("]\n");
+        double minimum = 1; // should be changed to the maximum possible value
+        for (int p_ind = 0; p_ind < number_of_points_; p_ind++){
+            double temp_array[dim_];
+            for (int p_dim = 0; p_dim < dim_; p_dim++){
+                temp_array[p_dim] = temp_grid_point[p_dim] - points[p_ind][p_dim];
+            }
+            double p_dist = cblas_dnrm2((int) dim_, temp_array, 1);
+            if (p_dist < minimum){
+                minimum = p_dist;
+            }
+        }
+        // increase the grid by one step
+        temp_grid_point[0] += threshold;
+        for (int d=1; d<dim_; d++) {
+            if ((temp_i+1)%partial_temp_points[d]==0){
+                temp_grid_point[d] += threshold;
+                if (d != 0){
+                    temp_grid_point[d-1] = min_vector[d-1];
+                }
+            }
+        } // end increase
+
+        if (minimum > maximum){
+            maximum = minimum;
+        }
+    }
+    return maximum;
+}
 
 int main(){
-    file_pointer = fopen("/app/home/lotf/CLionProjects/parallel_multiscale_approximation/data_2D_few.csv", "r");
-    char data_line[100];
-    const size_t total_number_of_points = 10000; // TO DO: implement the detection of this value
+    clock_t start_time = clock();
+    FILE *file_pointer;
+    file_pointer = fopen("/app/home/lotf/CLionProjects/parallel_multiscale_approximation/data_2D.csv", "r");
+    char data_line[100]; // enough to have all read digits in memory
 
     // find out the dimension
     fgets(data_line, sizeof data_line, file_pointer);
@@ -128,8 +198,16 @@ int main(){
         temp_dim ++;
         token = strtok(NULL, ",");
     }
-    const int dim = temp_dim;
-    printf("dim = %d\n", dim);
+    const unsigned int dim = temp_dim;
+    printf("dim: %d\n", dim);
+
+    int temp_number_of_points =1;
+    while (fgets(data_line, sizeof data_line, file_pointer)){
+        temp_number_of_points++;
+    }
+    const unsigned int total_number_of_points = temp_number_of_points;
+    printf("number of points: %d\n", total_number_of_points);
+
     // back flow to avoid data loss is needed!
     fseek(file_pointer, 0, SEEK_SET);
 
@@ -144,14 +222,12 @@ int main(){
     // read values from file and store them in the matrix defined above.
     while (fgets(data_line, sizeof data_line, file_pointer))
     {
-        puts(data_line);
         token = strtok(data_line, ",");
-        printf("token %s\n", token);
         temp_dim = 0;
         while (token != NULL)
         {
             data[index][temp_dim] = strtod(token, NULL);
-            printf("dd %f, dim %d, index %d\n", data[index][temp_dim], temp_dim, index);
+            // printf("dd %f, dim %d, index %d\n", data[index][temp_dim], temp_dim, index);
             token = strtok(NULL, ",");
             temp_dim ++;
         }
@@ -159,10 +235,22 @@ int main(){
 
     }
     fclose(file_pointer);
+    clock_t read_time = clock()-start_time;
+    printf("Data read in %f sec\n", (double) read_time/CLOCKS_PER_SEC);
+
+
     // here I have successfully imported the values (double format) from the file into the 2-pointer data!
+    double sep = separation_distance(data, dim, total_number_of_points);
+    clock_t separation_time = clock()- read_time;
+    printf("Separation distance: %f, evaluated in %f sec.\n", sep, (double) separation_time/CLOCKS_PER_SEC);
 
-    printf("end\n");
+    double fill = fill_distance(data, dim, total_number_of_points, 0.01);
+    clock_t fill_time = clock()- separation_time;
+    printf("Fill distance: %f, evaluated in %f sec.\n", fill, (double) fill_time/CLOCKS_PER_SEC);
 
 
+
+
+    free(data);
     return 0;
 }
